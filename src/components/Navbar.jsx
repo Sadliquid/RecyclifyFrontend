df4f97897777e04d84eb2944307664823a7479df
lincoln0623 <lincolnlim267@gmail.com>
2024-12-15 00:33:18 +0800

    removed max width properties in the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Box, Heading } from "@chakra-ui/react"
 
 function Navbar() {
     return (
-        <Box as="nav" bg="#4DCBA4" w="90%" maxW="1200px" m="20px auto 20px auto" p="10px" color="white" position="fixed" top="0" left="0" right="0" rounded="10px"
+        <Box as="nav" bg="#4DCBA4" w="90%" m="20px auto 20px auto" p="10px" color="white" position="fixed" top="0" left="0" right="0" rounded="10px"
             zIndex={1} >
             <Heading>RECYCLIFY</Heading>
         </Box>
