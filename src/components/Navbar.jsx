a8b190940f522f81c1ae7f35e0d630f60c561cdb
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-01-11 02:51:28 +0800

    Removed unnecessary 400px ml on navbar hamburger icon

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Avatar } from "@/components/ui/avatar";
 function Navbar() {
     return (
         <Flex as="nav" bg="#4DCBA4" w="100%" p="8px 24px 8px 24px" rounded="10px" justify="space-between" align="center" alignItems="center">
-            <RxHamburgerMenu ml={"400px"} size="24px" color="white" cursor="pointer"/>
+            <RxHamburgerMenu size="24px" color="white" cursor="pointer"/>
             <Heading color="white" cursor="pointer">RECYCLIFY</Heading>
             <Avatar name="Joshua Long" src="https://replace.with.your.image.url" size="sm" cursor="pointer" />
         </Flex>
