dc3aaa7a5e78df65f9513366630b13ceecd61f55
lincoln0623 <lincolnlim267@gmail.com>
2025-01-11 02:21:17 +0800

    Added avatar ui components and update nav bar design. Added in Hamburger icon and Avatar icon with placeholder

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,16 @@
-import { Box, Heading } from "@chakra-ui/react"
-
+import { Flex, Heading, IconButton } from "@chakra-ui/react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Avatar } from "@/components/ui/avatar";
+import { ColorModeButton } from "./ui/color-mode";
 
 function Navbar() {
     return (
-        <Box as="nav" bg="#4DCBA4" w="90%" m="20px auto 20px auto" p="10px" color="white" position="fixed" top="0" left="0" right="0" rounded="10px"
-            zIndex={1} >
-            <Heading>RECYCLIFY</Heading>
-        </Box>
+        <Flex as="nav" bg="#4DCBA4" w="95%" m="20px auto 20px auto" p="10px 24px 10px 24px" position="fixed" top="0" left="0" right="0" rounded="10px" justify="space-between" align="center">
+            <RxHamburgerMenu ml={"400px"} size="24px" color="white" cursor="pointer"/>
+            <Heading color="white" cursor="pointer">RECYCLIFY</Heading>
+            <Avatar name="Joshua Long" src="https://replace.with.your.image.url" size="sm" cursor="pointer" />
+        </Flex>
     );
 }
 
-export default Navbar
+export default Navbar;
