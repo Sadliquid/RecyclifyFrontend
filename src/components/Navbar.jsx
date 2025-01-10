c90745b26db5dbefb847d777e061c4e8ecfa14c0
lincoln0623 <lincolnlim267@gmail.com>
2025-01-11 02:41:32 +0800

    Fix root layout issue and redesign flex box for the nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Avatar } from "@/components/ui/avatar";
 
 function Navbar() {
     return (
-        <Flex as="nav" bg="#4DCBA4" w="95%" m="20px auto 20px auto" p="10px 24px 10px 24px" position="fixed" top="0" left="0" right="0" rounded="10px" justify="space-between" align="center">
+        <Flex as="nav" bg="#4DCBA4" w="100%" p="8px 24px 8px 24px" rounded="10px" justify="space-between" align="center" alignItems="center">
             <RxHamburgerMenu ml={"400px"} size="24px" color="white" cursor="pointer"/>
             <Heading color="white" cursor="pointer">RECYCLIFY</Heading>
             <Avatar name="Joshua Long" src="https://replace.with.your.image.url" size="sm" cursor="pointer" />
