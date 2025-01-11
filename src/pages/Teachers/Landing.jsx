5921ebcc31e3683ec5d3fd334924f8c5abef94a8
lincoln0623 <lincolnlim267@gmail.com>
2025-01-12 03:21:01 +0800

    Added placeholder image for each diummy classes. Made few adjustment for the class

--- src/pages/Teachers/Landing.jsx
+++ src/pages/Teachers/Landing.jsx
@@ -6,13 +6,13 @@ function Landing() {
 
     // Dummy class data
     const classes = [
-        // { id: 1, className: '201', year: 'Year 2 Class 1', image: 'https://via.placeholder.com/150', bgColor: '#96E2D6' },
-        // { id: 2, className: '301', year: 'Year 3 Class 1', image: 'https://via.placeholder.com/150', bgColor: '#AEC7ED' },
-        // { id: 3, className: '401', year: 'Year 4 Class 1', image: 'https://via.placeholder.com/150', bgColor: '#D9D9D9' },
+        { id: 1, className: '201', year: 'Year 2 Class 1', image: '../../../src/assets/class1.jpg', bgColor: '#96E2D6' },
+        { id: 2, className: '301', year: 'Year 3 Class 1', image: '../../../src/assets/class2.jpg', bgColor: '#AEC7ED' },
+        { id: 3, className: '401', year: 'Year 4 Class 1', image: '../../../src/assets/class3.jpg', bgColor: '#D9D9D9' },
     ];
 
     return (
-        <Stack gap="8" direction="row" wrap="wrap" justify="center" mt={8}>
+        <Stack gap="8" direction="row" wrap="wrap" justify="center" mt={8} >
             <For
                 each={classes}
                 fallback={
@@ -22,13 +22,13 @@ function Landing() {
                     </VStack>}
             >
                 {(classItem, index) => (
-                    <Box key={index}>
-                        <Card.Root minW="lg" overflow="hidden" bg={classItem.bgColor} p="4">
+                    <Box key={index} >
+                        <Card.Root minW="lg" overflow="hidden" bg={classItem.bgColor} borderRadius="3xl">
                             <Card.Body >
-                                <Card.Title >{classItem.className}</Card.Title>
-                                <Card.Description>{classItem.year}</Card.Description>
+                                <Card.Title ><Text fontSize="2xl" fontWeight='bold'>{classItem.className}</Text></Card.Title>
+                                <Card.Description><Text color="black">{classItem.year}</Text></Card.Description>
                             </Card.Body>
-                            <Image src={classItem.image} alt={classItem.year} />
+                            <Image src={classItem.image} alt={classItem.year} fit="cover" maxW="lg" maxH="200px"/>
                         </Card.Root>
                     </Box>
                 )}
