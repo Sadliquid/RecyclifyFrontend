d8ada4d34881a7966a0bd1b3f9f4b370fd839025
JunHam <joonjunhan@gmail.com>
2025-01-13 02:56:09 +0800

    Added skeleton ui for parent and student registration forms

--- src/components/identity/ParentRegisterForm.jsx
+++ src/components/identity/ParentRegisterForm.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { VStack, Box, Button, Input, Text, Link, Flex } from "@chakra-ui/react";
 
-function ParentRegisterForm() {
-  return (
-    <div>ParentRegisterForm</div>
-  )
+function ParentRegistrationForm({ goBack }) {
+    return (
+        <Flex direction="column" align="center" width="100%" p={4} mt={5}>
+            <Flex width="100%" justify="flex-start" mb={4}>
+                <Button variant="ghost" onClick={goBack}>
+                    ← Back
+                </Button>
+            </Flex>
+            <Box width="100%" maxWidth="350px">
+                <VStack spacing={4} align="stretch">
+                    <Input placeholder="Name" />
+                    <Input placeholder="Email" />
+                    <Input placeholder="Child's Student ID" />
+                    <Input placeholder="Password" type="password" />
+                    <Input placeholder="Confirm Password" type="password" />
+                    <Button
+                        variant="solid"
+                        background="#2D65FF"
+                        color={"white"}
+                        width="50%"
+                        type="submit"
+                        borderRadius={30}
+                        alignSelf="center"
+                    >
+                        Get Started!
+                    </Button>
+                </VStack>
+            </Box>
+        </Flex>
+    );
 }
 
-export default ParentRegisterForm
+export default ParentRegistrationForm;
