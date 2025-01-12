ae36f187430c53b14b05f680baa25c5a93506939
JunHam <joonjunhan@gmail.com>
2025-01-13 00:18:29 +0800

    Added Login and createAccount pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,18 +6,21 @@ import React from 'react'
 import './index.css'
 
 // Page routes go here.
+import Login from './pages/Identity/Login'
+import CreateAccount from './pages/Identity/CreateAccount'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />} >
-
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  	<React.StrictMode>
+		<Provider>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />} >
+						<Route path={"login"} element={<Login />} />
+						<Route path={"createAccount"} element={<CreateAccount />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</Provider>
+  	</React.StrictMode>
 )
