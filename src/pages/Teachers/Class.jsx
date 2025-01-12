26082ab91010327dcb0e48fedef9bdf704d368c2
lincoln0623 <lincolnlim267@gmail.com>
2025-01-12 23:48:32 +0800

    Added Class Dashboard Layout

--- src/pages/Teachers/Class.jsx
+++ src/pages/Teachers/Class.jsx
@@ -40,7 +40,41 @@ function Class() {
                     <Tabs.Trigger value='Students'><PiStudentFill />Students Dashboard</Tabs.Trigger>
                 </Tabs.List>
                 <Tabs.Content value='Class'>
-                    <Text>Class Dashboard</Text>
+                    <Box w="100%" h="65dvh" p={4} bg="#9F9FF8" borderRadius="xl" boxShadow="md">
+                        <Flex gap={4} w="100%" h="100%" padding={4} align="center">
+                            <Flex w="80%" h="100%" direction="column" gap={4}>
+                                <Flex gap={4} w="100%" h="50%">
+                                    <Box w="70%" h="100%" p={4} bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                        Students Contribution
+                                    </Box>
+                                    <Box w="30%" h="100%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                        Class Top Contributors
+                                    </Box>
+                                </Flex>
+                                <Flex gap={4} w="100%" h="50%">
+                                    <Flex gap={4} w="70%" h="100%" direction="row">
+                                        <Box w="70%" h="100%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                            Month Contribution
+                                        </Box>
+                                        <Flex w="30%" h="100%" gap={4} direction="column" >
+                                            <Box w="100%" h="50%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                                Total Class Covers
+                                            </Box >
+                                            <Box  w="100%" h="50%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                                Weekly Class Covers
+                                            </Box>
+                                        </Flex>
+                                    </Flex>
+                                    <Box w="30%" h="100%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center">
+                                        Flagged Student
+                                    </Box>
+                                </Flex>
+                            </Flex>
+                            <Box w="20%" h="100%" bg="white" borderRadius="xl" boxShadow="md" color="black" textAlign="center" display="flex" alignItems="center" justifyContent="center" >
+                                Leaderboards
+                            </Box>
+                        </Flex>
+                    </Box>
                 </Tabs.Content>
                 <Tabs.Content value='Students'>
                     <Text>Students Dashboard</Text>
