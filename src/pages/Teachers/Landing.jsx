dc796c5b45bc55e428c0d4f255fc8e859fd8322b
lincoln0623 <lincolnlim267@gmail.com>
2025-01-12 16:21:13 +0800

    Imported menu ui component and improved teacher's landing page responsiveness.
    Added class card menu options without functionality and add class button without functionallity

--- src/pages/Teachers/Landing.jsx
+++ src/pages/Teachers/Landing.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
-import { Box, Card, Image, For, Stack, VStack, Text } from '@chakra-ui/react'
+import { Box, Card, Image, For, Stack, VStack, Text, Button, Flex, useBreakpointValue } from '@chakra-ui/react'
 import { LuBox } from "react-icons/lu"
+import { IoIosAddCircle } from "react-icons/io";
 import { motion } from 'framer-motion'
+import { MdOutlineContentCopy, MdEdit, MdDelete, MdOutlineMoreVert, MdAddCircle } from "react-icons/md";
+import {
+    MenuContent,
+    MenuItem,
+    MenuRoot,
+    MenuTrigger,
+} from "@/components/ui/menu"
 
 function Landing() {
 
@@ -12,36 +20,72 @@ function Landing() {
         { id: 3, className: '401', year: 'Year 4 Class 1', image: '../../../src/assets/class3.jpg', bgColor: '#D9D9D9' },
     ];
 
+    // Card size based on screen size
+    const cardSize = useBreakpointValue({
+        base: '2xs',
+        md: 'md',
+        lg: 'lg',
+    });
+
     return (
-        <Stack gap="8" direction="row" wrap="wrap" justify="center" mt={8} >
-            <For
-                each={classes}
-                fallback={
-                    <VStack textAlign="center" fontWeight="medium">
-                        <LuBox />
-                        <Text>No class data available</Text>
-                    </VStack>}
-            >
-                {(classItem, index) => (
-                    <motion.div
-                        key={index}
-                        initial={{ y: 0, opacity: 1 }}
-                        whileHover={{ y: -5, opacity: 0.9 }}
-                        transition={{ duration: 0.2 }}
-                    >
-                        <Box key={index} >
-                            <Card.Root minW="lg" overflow="hidden" bg={classItem.bgColor} borderRadius="3xl">
-                                <Card.Body >
-                                    <Card.Title fontWeight="bold" fontSize="3xl" mb={6}>{classItem.className}</Card.Title>
-                                    <Card.Description color="black">{classItem.year}</Card.Description>
-                                </Card.Body>
-                                <Image src={classItem.image} alt={classItem.year} fit="cover" maxW="lg" maxH="200px" />
-                            </Card.Root>
-                        </Box>
-                    </motion.div>
-                )}
-            </For>
-        </Stack>
+        <>
+            <Stack gap="8" direction="row" wrap="wrap" justify="center" mt={8} >
+                <For
+                    each={classes}
+                    fallback={
+                        <VStack textAlign="center" fontWeight="medium">
+                            <LuBox />
+                            <Text>No class data available</Text>
+                        </VStack>}
+                >
+                    {(classItem, index) => (
+                        <motion.div
+                            key={index}
+                            initial={{ y: 0, opacity: 1 }}
+                            whileHover={{ y: -5, opacity: 0.9 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <Box key={index} >
+                                <Card.Root minW={cardSize} overflow="hidden" bg={classItem.bgColor} borderRadius="3xl">
+                                    <Card.Body >
+                                        <Flex justify={'space-between'} align={'center'} mb={4} wrap={'wrap'} >
+                                            <Card.Title fontWeight="bold" fontSize="3xl">{classItem.className}</Card.Title>
+                                            <MenuRoot positioning={{ placement: "bottom-end" }}>
+                                                <MenuTrigger asChild>
+                                                    <MdOutlineMoreVert size={24} color="black" />
+                                                </MenuTrigger>
+                                                <MenuContent borderRadius="xl">
+                                                    <MenuItem value="copy-uuid" borderRadius="xl">
+                                                        <MdOutlineContentCopy /> Copy UUID
+                                                    </MenuItem>
+                                                    <MenuItem value="edit-class" borderRadius="xl">
+                                                        <MdEdit /> Edit
+                                                    </MenuItem>
+                                                    <MenuItem value="delete-class" bg="#FF8080" borderRadius="xl">
+                                                        <MdDelete /> Delete
+                                                    </MenuItem>
+                                                </MenuContent>
+                                            </MenuRoot>
+                                        </Flex>
+                                        <Card.Description color="black">{classItem.year}</Card.Description>
+                                    </Card.Body>
+                                    <Image src={classItem.image} alt={classItem.year} fit="cover" maxW="100%" maxH="200px" />
+                                </Card.Root>
+                            </Box>
+                        </motion.div>
+                    )}
+                </For>
+            </Stack>
+
+            {/* Add new class button */}
+            <IoIosAddCircle position="fixed"  size={50} color="#92BFFF" aria-label="Add Class" cursor="pointer"
+            style={{
+                position: 'fixed',
+                bottom: '40px',
+                right: '40px',
+                zIndex: 1000,
+            }}/>
+        </>
     )
 }
 
