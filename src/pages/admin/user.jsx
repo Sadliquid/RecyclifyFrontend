e53c57bd8c482b9dcdd2361b796e5df7ee205c4d
Nicholas Chew <3xpect1916@gmail.com>
2025-01-13 15:28:17 +0800

    static data edited missing edit button

--- src/pages/admin/user.jsx
+++ src/pages/admin/user.jsx
@@ -23,17 +23,21 @@ const UserManagement = () => {
       <Table.Root size="sm" variant="outline">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeader>Product</Table.ColumnHeader>
-            <Table.ColumnHeader>Category</Table.ColumnHeader>
-            <Table.ColumnHeader textAlign="end">Price</Table.ColumnHeader>
+            <Table.ColumnHeader>Profile Photo</Table.ColumnHeader>
+            <Table.ColumnHeader>Name</Table.ColumnHeader>
+            <Table.ColumnHeader>Email</Table.ColumnHeader>
+            <Table.ColumnHeader>Phone Number</Table.ColumnHeader>
+            <Table.ColumnHeader>Role</Table.ColumnHeader>
           </Table.Row>
         </Table.Header>
         <Table.Body>
           {filteredItems.map((item) => (
             <Table.Row key={item.id}>
+              <Table.Cell>{item.profilePhoto}</Table.Cell>
               <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>{item.category}</Table.Cell>
-              <Table.Cell textAlign="end">{item.price.toFixed(2)}</Table.Cell>
+              <Table.Cell>{item.email}</Table.Cell>
+              <Table.Cell>{item.phone}</Table.Cell>
+              <Table.Cell>{item.role}</Table.Cell>
             </Table.Row>
           ))}
         </Table.Body>
@@ -43,11 +47,11 @@ const UserManagement = () => {
 };
 
 const items = [
-  { id: 1, name: "Laptop", category: "Electronics", price: 999.99 },
-  { id: 2, name: "Coffee Maker", category: "Home Appliances", price: 49.99 },
-  { id: 3, name: "Desk Chair", category: "Furniture", price: 150.0 },
-  { id: 4, name: "Smartphone", category: "Electronics", price: 799.99 },
-  { id: 5, name: "Headphones", category: "Accessories", price: 199.99 },
+  { id: 1, profilePhoto: "hehe", name: "Nicholas Chew", email: "Electronics", phone: "12345678", role: "Student" },
+  { id: 2, profilePhoto: "hehe", name: "Coffee Maker", email: "Home Appliances", phone: "12345678", role: "Student" },
+  { id: 3, profilePhoto: "hehe", name: "Desk Chair", email: "Furniture", phone: "12345678", role: "Student" },
+  { id: 4, profilePhoto: "hehe", name: "Smartphone", email: "Electronics", phone: "12345678", role: "Student" },
+  { id: 5, profilePhoto: "hehe", name: "Headphones", email: "Accessories", phone: "12345678", role: "Student" },
 ];
 
 export default UserManagement;
