6e68280ab4173b282a22dd81c6efb2da4aa0c3af
Nicholas Chew <3xpect1916@gmail.com>
2025-01-13 16:16:26 +0800

    finished inventory static

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import React from 'react'
 import './index.css'
 import Dashboard from './pages/admin/landing'
 import UserManagement from './pages/admin/user'
+import InventoryManagement from './pages/admin/inventory'
 
 // Page routes go here.
 
@@ -18,6 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path="/" element={<Layout />} >
             <Route path="/admin" element={<Dashboard />} ></Route>
             <Route path="/admin/user_management" element={<UserManagement />} > /</Route>
+            <Route path="/admin/inventory_management" element={<InventoryManagement />} > /</Route>
           </Route>
         </Routes>
       </BrowserRouter>
