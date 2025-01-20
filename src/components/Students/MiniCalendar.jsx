cedc75c2b5c4fd8fcdb7ecfd0799acda145bf1ec
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-01-20 14:54:24 +0800

    Added dynamic streak counting for calender highlights

--- src/components/Students/MiniCalendar.jsx
+++ src/components/Students/MiniCalendar.jsx
@@ -11,52 +11,55 @@ import { styled } from '@mui/material/styles';
 dayjs.extend(isBetweenPlugin);
 
 const CustomPickersDay = styled(PickersDay, {
-    shouldForwardProp: (prop) => prop !== 'isSelected',
-  })(({ isSelected, isToday, isDisabled }) => ({
-    ...(isSelected && {
-      backgroundColor: isToday ? '#CCAC00' : '#E5D200',
-      color: '#ffffff',
-      '&:hover, &:focus': {
-        backgroundColor: isToday ? '#b89700' : '#D4C400',
-      },
-    }),
-    '&.Mui-selected': {
-      outline: 'none',
-    },
-    '&:focus': {
-      outline: 'none',
-    },
-    '&.MuiPickersDay-today': {
-      border: '2px solid #3DA287',
-    },
-    pointerEvents: isDisabled ? 'none' : 'auto',
-    color: isSelected ? 'white' : '#B0B0B0',
+	shouldForwardProp: (prop) => prop !== 'isSelected',
+})(({ isSelected, isToday, isDisabled }) => ({
+	...(isSelected && {
+		backgroundColor: isToday ? '#CCAC00' : '#E5D200',
+		color: '#ffffff',
+		'&:hover, &:focus': {
+			backgroundColor: isToday ? '#b89700' : '#D4C400',
+		},
+	}),
+	'&.Mui-selected': {
+		outline: 'none',
+	},
+	'&:focus': {
+		outline: 'none',
+	},
+	'&.MuiPickersDay-today': {
+		border: '2px solid #3DA287',
+	},
+	pointerEvents: isDisabled ? 'none' : 'auto',
+	color: isSelected ? 'white' : '#B0B0B0',
 }));
-  
-  function isInSameWeek(day) {
-    return day.isSame(dayjs(), 'week');
-  }
-  
-  function MiniCalendar({ streak }) {
+
+function isHighlighted(day, streak) {
+    const today = dayjs();
+    const streakStart = today.subtract(streak - 1, 'day');
+    return day.isBetween(streakStart, today, 'day', '[]');
+}
+
+function MiniCalendar({ streak }) {
     return (
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DateCalendar']}>
-          <DemoItem label={`Your current streak: ${streak}`}>
-            <DateCalendar
-              slots={{
-                day: (dayProps) => (
-                  <CustomPickersDay
-                    {...dayProps}
-                    isSelected={isInSameWeek(dayProps.day)}
-                    isDisabled={true}
-                  />
-                ),
-              }}
-            />
-          </DemoItem>
-        </DemoContainer>
-      </LocalizationProvider>
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer components={['DateCalendar']}>
+                <DemoItem label={`Your current streak: ${streak}`}>
+                    <DateCalendar
+                        slots={{
+                            day: (dayProps) => (
+                                <CustomPickersDay
+                                    {...dayProps}
+                                    isSelected={isHighlighted(dayProps.day, streak)}
+                                    isDisabled={false}
+                                />
+                            ),
+                        }}
+                    />
+                </DemoItem>
+            </DemoContainer>
+        </LocalizationProvider>
     );
-  }
-  
-  export default MiniCalendar;  
+}
+
+
+export default MiniCalendar;  
