ab53dc7b0ef9d163ba49ad4f573c74da340a781b
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-01-21 15:49:32 +0800

    Redesigned leaderboard layout

--- src/pages/Students/Leaderboards.jsx
+++ src/pages/Students/Leaderboards.jsx
@@ -7,13 +7,13 @@ function Leaderboards() {
         <Box display="flex" justifyContent={"center"} flexDir="column" mt={10} width={"100%"}>
             <Heading fontSize="30px">Leaderboards</Heading>
 
-            <Box display="flex" justifyContent={"space-between"} width="100%" height={"77vh"} mt={10} boxSizing={"border-box"}>
+            <Box display="flex" justifyContent={"space-between"} width="100%" height={"70vh"} mt={10} boxSizing={"border-box"}>
                 <Box display="flex" flexDir={"column"} justifyContent={"space-between"} width="28%">
-                    <Box display="flex" flexDir={"column"} justifyContent={"space-around"} alignItems={"center"} backgroundColor="#E5ECFF" borderRadius={20} height="85%">
+                    <Box display="flex" flexDir={"column"} justifyContent={"space-around"} alignItems={"center"} backgroundColor="#E5ECFF" borderRadius={20} height="85%" padding={2}>
                         <Avatar boxSize="150px" src="https://bit.ly/dan-abramov" />
                         <Heading fontSize={"30px"} mt={2}>Liew Jun Han</Heading>
                         <Heading color="#2CD776">500 Leafs</Heading>
-                        <Box display="flex" justifyContent={"center"} alignItems={"center"} border="3px solid #4DCBA4" borderRadius={20} width="90%" height="20%" mt={2}>
+                        <Box display="flex" justifyContent={"center"} alignItems={"center"} border="3px solid #4DCBA4" borderRadius={20} width="97%" height="20%" mt={2}>
                             <Box mr={2}>
                                 <Image src="/silver-medal.png" boxSize={14}/>
                             </Box>
@@ -24,20 +24,12 @@ function Leaderboards() {
                             </Box>
                         </Box>
 
-                        <Box display="flex" justifyContent={"center"} alignItems={"center"} border="3px solid #4DCBA4" borderRadius={20} width="90%" height="20%" padding={5}>
-                            <Box mr={2}>
-                                <Image src="/silver-medal.png" boxSize={14}/>
-                            </Box>
-
-                            <Box display="flex" flexDir={"column"} ml={2} mt={-2} >
-                                <Text textAlign={"left"} fontSize={"md"}>Finish in the top 3 weekly to advance to the Gold League</Text>
-                            </Box>
-                        </Box>
+                        <Text fontFamily={"Lilita One"} color={"charcoal"}>Finish in the top 3 weekly to advance to the Gold League!</Text>
                     </Box>
 
                     <Box display="flex" justifyContent={"center"} alignItems={"center"} backgroundColor="#4DCBA4" borderRadius={20} height="13%">
-                        <Text as={LuTimer} fontSize="60px" color={"white"} mr={2} />
-                        <Heading fontSize={"30px"} color="white" ml={2}>2 Days, 8 Hours Left</Heading>
+                        <Text as={LuTimer} fontSize="40px" color={"white"} mr={2} />
+                        <Heading fontSize={"20px"} color="white" ml={2}>2 Days, 8 Hours Left</Heading>
                     </Box>
                 </Box>
 
