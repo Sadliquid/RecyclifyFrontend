4c981f56f983a0c5a889a3cf6fd201baa8f7ff92
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-02-13 04:54:08 +0800

    Added student inbox

--- src/pages/Students/StudentInbox.jsx
+++ src/pages/Students/StudentInbox.jsx
@@ -1,18 +1,68 @@
-import { Box, Heading } from '@chakra-ui/react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { Box, Heading, Tabs, Badge } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import server from '../../../networking';
+import ShowToast from '../../Extensions/ShowToast';
+import NotificationRow from '../../components/Students/NotificationRow';
 
 function StudentInbox() {
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 10 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5 }}
-        >
-            <Box display="flex" justifyContent={"center"} flexDir="column" mt={10}>
-                <Heading fontSize="30px">Inbox</Heading>
-            </Box>
-        </motion.div>
-    )
+    const { user, loaded, error } = useSelector((state) => state.auth);
+    const [inboxMessages, setInboxMessages] = useState([]);
+
+    const fetchMessages = async (studentID) => {
+        try {
+            const response = await server.get(`/api/student/get-student-inbox-messages/?studentID=${studentID}`);
+            if (response.status === 200) {
+                const messages = response.data.data;
+                setInboxMessages(messages);
+            }
+        } catch (error) {
+            if (error.response && error.response.data && error.response.data.error && typeof error.response.data.error === "string") {
+                if (error.response.data.error.startsWith("UERROR")) {
+                    ShowToast("error", error.response.data.error.substring("UERROR:".length));
+                    return;
+                } else {
+                    ShowToast("error", error.response.data.error.substring("ERROR:".length));
+                    return;
+                }
+            }
+        }
+    };
+
+    useEffect(() => {
+        if (!error && loaded && user && user.userRole == "student") {
+            fetchMessages(user.id);
+        }
+    }, [loaded]);
+
+    if (!error && loaded && user) {
+
+        return (
+            <motion.div
+                initial={{ opacity: 0, y: 10 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5 }}
+            >
+                <Box display="flex" justifyContent={"center"} flexDir="column" mt={10} mb={10}>
+                    <Heading fontSize="30px">Inbox</Heading>
+                </Box>
+
+                <Tabs.Root defaultValue="All" colorPalette={"blue"}>
+                    <Tabs.List>
+                        <Tabs.Trigger value="All" bg="none">
+                            All {inboxMessages.length > 0 && <Badge ml={2}>{inboxMessages.length}</Badge>}
+                        </Tabs.Trigger>
+                    </Tabs.List>
+
+                    <Tabs.Content value="All">
+                        {inboxMessages.map((message) => <NotificationRow key={message.id} message={message} />)}
+                    </Tabs.Content>
+                </Tabs.Root>
+            </motion.div>
+        )
+    }
 }
 
 export default StudentInbox
