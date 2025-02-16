8c2ca99b63dc2a0c7aeeca23fc4a46578b4a7959
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-02-16 22:37:28 +0800

    email newsletter

--- src/pages/parents/EmailNewsletter.jsx
+++ src/pages/parents/EmailNewsletter.jsx
@@ -4,10 +4,33 @@ import { Switch } from "@/components/ui/switch"
 import { useEffect } from "react"
 import { useSelector } from "react-redux"
 import { useState } from "react"
+import ShowToast from "../../Extensions/ShowToast"
+import server from "../../../networking"
 
 const EmailNewsletter = () => {
     const [checked, setChecked] = useState(false)
     const { user, loaded, error } = useSelector((state) => state.auth);
+
+    const handleSubmit = async () => {
+        if (!error && loaded && user) {
+            try {
+                const response = await server.get(`/api/Events/send-email-newsletter?userID=${user.id}`);
+                if (response.status === 200) {
+                    ShowToast("success", "Email newsletter sent successfully.")
+                }
+            } catch (error) {
+                if (error.response && error.response.data && error.response.data.error && typeof error.response.data.error === "string") {
+                    if (error.response.data.error.startsWith("UERROR")) {
+                        ShowToast("error", error.response.data.error.substring("UERROR: ".length))
+                    } else {
+                        ShowToast("error", error.response.data.error.substring("ERROR: ".length))
+                    }
+                } else {
+                    ShowToast("error", "An unknown error occurred.")
+                }
+            }
+        }
+    }
 
     useEffect(() => {
         if (!error && loaded && user && user.userRole == "parent") {
@@ -25,7 +48,7 @@ const EmailNewsletter = () => {
                     <Switch checked={checked} onCheckedChange={(e) => setChecked(e.checked)} />
                 </Box>
 
-                <Button disabled={!checked} backgroundColor={"#4DCBA4"} _hover={{ backgroundColor: "#3CA685" }} color="white" px={8} py={4} borderRadius={24} fontWeight="bold" fontSize="md">
+                <Button disabled={!checked} backgroundColor={"#4DCBA4"} _hover={{ backgroundColor: "#3CA685" }} color="white" px={8} py={4} borderRadius={24} fontWeight="bold" fontSize="md" onClick={() => handleSubmit()}>
                     Re-send newsletter
                 </Button>
             </Stack>
