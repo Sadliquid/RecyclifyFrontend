957bc9a55e3049795bdf9c9f373cc67dc4c31f56
Nicholas Chew <3xpect1916@gmail.com>
2025-02-17 11:29:42 +0800

    Added routes to contact us and ecopilot page on parents faq page

--- src/pages/parents/Faq.jsx
+++ src/pages/parents/Faq.jsx
@@ -1,11 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot } from "@/components/ui/accordion"
-import { Heading, Box } from "@chakra-ui/react"
-import { useEffect } from "react"
-import { useSelector } from "react-redux"
+import { AccordionItem, AccordionItemContent, AccordionItemTrigger, AccordionRoot } from "@/components/ui/accordion";
+import { Heading, Box, Button, Flex, Text, VStack } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 
 const Faq = () => {
+    const navigate = useNavigate();
     const { user, loaded, error } = useSelector((state) => state.auth);
+
 
     const questions = [
         { value: "1", title: "How are points calculated within the application?", text: "The points are calculated based on how students perform in each task. Some tasks are done by the student themselves while others are done by the class in a weekly basis." },
@@ -14,12 +18,13 @@ const Faq = () => {
         { value: "4", title: "How do I look through individual events?", text: "Click on the “More” icon for each event." },
         { value: "5", title: "What is the purpose of Recyclify? How is Recyclify productive?", text: "The purpose of Recyclify is to engage students in recycling sustainably through gamification. It is productive because it engages and encourages the student to contribute to environmental progress while making it fun." },
     ];
-    
+   
     useEffect(() => {
         if (!error && loaded && user && user.userRole == "parent") {
             console.log("User logged in as parent with ID:", user.id);
         }
     }, [loaded]);
+
 
     return (
         <Box>
@@ -32,8 +37,15 @@ const Faq = () => {
                     </AccordionItem>
                 ))}
             </AccordionRoot>
+            <VStack mt={8} spacing={4} align="center">
+                <Text fontSize="lg" fontWeight="medium">For further assistance, you can reach us here:</Text>
+                <Button bg={"#4DCBA4"} onClick={() => navigate("/contact")} width="200px">Go to Contact Page</Button>
+                <Text fontSize="lg" fontWeight="medium">Alternative you can ask our own chatbot EcoPilot for any queries!</Text>
+                <Button bg={"#4DCBA4"} onClick={() => navigate("/ecoPilot")} width="200px">Go to EcoPilot page</Button>
+            </VStack>
         </Box>
-    )
+    );
 }
+
 
 export default Faq;
