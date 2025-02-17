e2a2c57521796147f341432e53cae7cd7690ce75
Joshua Long Yu Xuan <joshu5739yx@gmail.com>
2025-02-17 17:28:06 +0800

    fixed formatting for student inbox

--- src/components/Students/NotificationRow.jsx
+++ src/components/Students/NotificationRow.jsx
@@ -4,31 +4,24 @@ import { Avatar } from "@/components/ui/avatar";
 
 const NotificationRow = ({ message }) => {
     return (
-        <>
-            <Flex direction="row" align="center" justify="space-between" w="100%" key={message.id} p={4} position="relative"
-                borderRadius="md" mb={2} justifyContent="space-between" alignItems="center" bg={"gray.100"}>
-                <Box display="flex" alignItems="center" gap={2} width={"50%"} >
-
-                    <Box w="10%" h="100%" display="flex" justifyContent="center" alignItems="center">
-                        <Avatar src={"/defaultPfp.jpg"} size="sm" cursor="pointer" />
-                    </Box>
-
-                    <Text noOfLines={1} isTruncated>
-                        <Text as="span" fontWeight="bold">
-                            {message.message}
-                        </Text>
+        <Flex direction="row" align="center" justify="space-between" w="100%" key={message.id} p={4} position="relative"
+            borderRadius="md" mb={2} bg={"gray.100"}>
+            <Box display="flex" alignItems="center" gap={2} flex={1}>
+                <Box w="40px" h="40px" display="flex" justifyContent="center" alignItems="center">
+                    <Avatar src={"/defaultPfp.jpg"} size="sm" cursor="pointer" />
+                </Box>
+                <Text fontWeight="bold" whiteSpace="normal" wordBreak="break-word">
+                    {message.message}
+                </Text>
+            </Box>
+            <Box display="flex" flexDirection="column" alignItems="flex-end">
+                {message.date && (
+                    <Text fontSize="sm" fontWeight="bold" color="gray.800">
+                        {message.date}
                     </Text>
-                </Box>
-
-                <Box display="flex" flexDirection="column" alignItems="flex-end">
-                    {message.date && (
-                        <Text fontSize="sm" fontWeight="bold" color="gray.800">
-                            {message.date}
-                        </Text>
-                    )}
-                </Box>
-            </Flex>
-        </>
+                )}
+            </Box>
+        </Flex>
     );
 };
 
